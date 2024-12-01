a3a951a9c9c8ec5b7bf8707729191858668baf73
Stefan VDV <vdvman88@gmail.com>
2024-12-02 08:51:41 +1100

    feat: 🎉 Setup AoC template
    Includes benchmarking and automatically embedding the input data for each day and part

---
+++ Aoc2024/DayBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2024
+{
+    public abstract class DayBase
+    {
+        public DayBase() => ParseData();
+
+        public abstract void ParseData();
+
+        public abstract string Solve1();
+
+        public abstract string Solve2();
+    }
+}
