3aa1dfbf51578a9c6fa9f6c1d7657b87a5c115de
Stefan VDV <vdvman88@gmail.com>
2024-12-07 22:03:24 +1100

    feat: ✨ Add helper for copying a list of arrays to another list of arrays of the same shape

---
+++ Aoc2024/Utilities/ListExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Aoc2024.Utilities;
+
+public static class ListExtensions
+{
+    public static void CopyTo2d<T>(this List<T[]> source, List<T[]> destination)
+    {
+        for (int i = 0; i < source.Count; i++)
+        {
+            source[i].CopyTo(destination[i], 0);
+        }
+    }
+}
