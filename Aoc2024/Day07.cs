f2b2e2ec6ea7534cf3e8a880318485c0431d8316
Stefan VDV <vdvman88@gmail.com>
2024-12-07 23:52:37 +1100

    feat: ✨ Solve day 7 part 2

--- Aoc2024/Day07.cs
+++ Aoc2024/Day07.cs
@@ -10,19 +10,7 @@ public partial class Day07 : DayBase
     [Benchmark]
     public override void ParseData()
     {
-        var parser = new Parser(Contents
-        //"""
-        //190: 10 19
-        //3267: 81 40 27
-        //83: 17 5
-        //156: 15 6
-        //7290: 6 8 6 15
-        //161011: 16 10 13
-        //192: 17 8 14
-        //21037: 9 7 18 13
-        //292: 11 6 16 20
-        //"""u8
-        );
+        var parser = new Parser(Contents);
         Equations.Clear();
 
         while (!parser.IsEmpty && parser.ParseLine() is var lineParser)
@@ -70,6 +58,28 @@ public partial class Day07 : DayBase
     [Benchmark]
     public override string Solve2()
     {
-        throw new NotImplementedException();
+        long sum = 0;
+
+        foreach (var (result, terms) in Equations)
+        {
+            if (HasSolution2(result, terms[0], CollectionsMarshal.AsSpan(terms)[1..]))
+            {
+                sum += result;
+            }
+        }
+
+        return sum.ToString();
+    }
+
+    private static bool HasSolution2(long goal, long partialResult, ReadOnlySpan<int> remainingTerms)
+    {
+        if (remainingTerms.Length == 0)
+        {
+            return partialResult == goal;
+        }
+
+        return HasSolution2(goal, partialResult + remainingTerms[0], remainingTerms[1..])
+            || HasSolution2(goal, partialResult * remainingTerms[0], remainingTerms[1..])
+            || HasSolution2(goal, long.Parse(partialResult.ToString() + remainingTerms[0]), remainingTerms[1..]);
     }
 }
