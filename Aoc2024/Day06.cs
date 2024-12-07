50a4e85773c7331960c1de240f5956abb21af4aa
Stefan VDV <vdvman88@gmail.com>
2024-12-07 22:03:50 +1100

    feat: ✨ Solve day 6 part 2

--- Aoc2024/Day06.cs
+++ Aoc2024/Day06.cs
@@ -1,4 +1,6 @@
 ﻿using Aoc2024.Utilities;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace Aoc2024;
 
@@ -18,20 +20,7 @@ public partial class Day06 : DayBase
     [Benchmark]
     public override void ParseData()
     {
-        var parser = new Parser(Contents
-        //"""
-        //....#.....
-        //.........#
-        //..........
-        //..#.......
-        //.......#..
-        //..........
-        //.#..^.....
-        //........#.
-        //#.........
-        //......#...
-        //"""u8
-        );
+        var parser = new Parser(Contents);
         Map.Clear();
 
         int y = 0;
@@ -107,7 +96,7 @@ public partial class Day06 : DayBase
                     goto case Cell.GuardVisited;
                 case Cell.GuardVisited:
                     pos = nextPos;
-                    nextPos = nextPos + direction;
+                    nextPos += direction;
                     break;
             }
         }
@@ -115,9 +104,92 @@ public partial class Day06 : DayBase
         return count.ToString();
     }
 
+    [InlineArray(4)]
+    private struct CellDirections
+    {
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Actually used as the inline array element zero")]
+        [SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "Not valid for inline array elements")]
+        private bool TravelledInDirection;
+    }
+
     [Benchmark]
     public override string Solve2()
     {
-        throw new NotImplementedException();
+        var originalVisitedMap = Map.ConvertAll(row => new CellDirections[row.Count]);
+        var pos = GuardStartPos;
+        var direction = GuardStartDirection;
+        originalVisitedMap[pos.Y][pos.X][direction.GetUnitHash()] = true;
+
+        // Avoid allocating a new map for every attempt
+        var attemptVisitedMap = originalVisitedMap.ConvertAll(row => row.ToArray());
+
+        var triedLocations = Map.ConvertAll(row => new bool[row.Count]);
+
+        var count = 0;
+        var nextPos = pos + direction;
+        while (0 <= nextPos.Y && nextPos.Y < Map.Count && 0 <= nextPos.X && Map[nextPos.Y] is var row && nextPos.X < row.Count)
+        {
+            var cell = row[nextPos.X];
+            // Don't bother attempting to place obstacles where one already exists, and don't attempt at the guard's starting position
+            if (cell == Cell.Empty)
+            {
+                ref bool tried = ref triedLocations[nextPos.Y][nextPos.X];
+                if (!tried)
+                {
+                    tried = true;
+                    originalVisitedMap.CopyTo2d(attemptVisitedMap);
+                    if (FormsLoop(attemptVisitedMap, pos, direction))
+                    {
+                        ++count;
+                    }
+                }
+            }
+
+            // Update original map with current path
+            if (cell == Cell.Obstacle)
+            {
+                direction = direction.Rotated90CW();
+                nextPos = pos + direction;
+            }
+            else
+            {
+                pos = nextPos;
+                nextPos += direction;
+            }
+            originalVisitedMap[pos.Y][pos.X][direction.GetUnitHash()] = true;
+        }
+
+        return count.ToString();
+    }
+
+    private bool FormsLoop(List<CellDirections[]> visitedMap, VectorI2d pos, VectorI2d direction)
+    {
+        var nextPos = pos + direction;
+        var tempObstaclePos = nextPos;
+        while (0 <= nextPos.Y && nextPos.Y < Map.Count && 0 <= nextPos.X && Map[nextPos.Y] is var row && nextPos.X < row.Count)
+        {
+            ref bool visited = ref visitedMap[nextPos.Y][nextPos.X][direction.GetUnitHash()];
+            if (visited)
+            {
+                return true;
+            }
+
+            if (nextPos == tempObstaclePos || row[nextPos.X] == Cell.Obstacle)
+            {
+                direction = direction.Rotated90CW();
+                // Also mark the new direction at the current position as visited
+                visitedMap[pos.Y][pos.X][direction.GetUnitHash()] = true;
+                nextPos = pos + direction;
+            }
+            else
+            {
+                pos = nextPos;
+                nextPos += direction;
+                // Mark the new position as visited in the current direction
+                visited = true;
+            }
+        }
+
+        return false;
     }
 }
