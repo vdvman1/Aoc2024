e1bd75b206e45b9df5ac682c268f10af519cc696
Stefan VDV <vdvman88@gmail.com>
2024-12-09 10:09:49 +1100

    feat: ✨ Add gcd maths helper

---
+++ Aoc2024/Utilities/MathPlus.cs
@@ -0,0 +1,21 @@
+﻿namespace Aoc2024.Utilities;
+
+public static class MathPlus
+{
+    /// <summary>
+    /// Calculate the greatest common divisor of two signed integers
+    /// </summary>
+    /// <remarks>
+    /// Based on the pseudocode at https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations
+    /// </remarks>
+    /// <returns>The positive greatest common divisor</returns>
+    public static int Gcd(int a, int b)
+    {
+        while (b != 0)
+        {
+            (a, b) = (b, a % b);
+        }
+
+        return Math.Abs(a);
+    }
+}
