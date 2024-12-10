de2cd58ccbb8b01d4b4c47dafb8b80c0f916f0de
Stefan VDV <vdvman88@gmail.com>
2024-12-10 14:34:35 +1100

    feat: ✨ Add list extension to shift elements within a list

--- Aoc2024/Utilities/ListExtensions.cs
+++ Aoc2024/Utilities/ListExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Aoc2024.Utilities;
+﻿using System.Runtime.InteropServices;
+
+namespace Aoc2024.Utilities;
 
 public static class ListExtensions
 {
@@ -8,5 +10,14 @@ public static class ListExtensions
         {
             source[i].CopyTo(destination[i], 0);
         }
+    }
+
+    public static ref T ShiftElement<T>(this List<T> list, int fromIndex, int toIndex)
+    {
+        var span = CollectionsMarshal.AsSpan(list);
+        var elem = span[fromIndex];
+        span[toIndex..fromIndex].CopyTo(span[(toIndex + 1)..(fromIndex + 1)]);
+        span[toIndex] = elem;
+        return ref span[toIndex];
     }
 }
