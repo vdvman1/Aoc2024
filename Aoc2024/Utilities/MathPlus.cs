dccfd4f3f0c89186f68dbe4671b8b972b4420cec
Stefan VDV <vdvman88@gmail.com>
2024-12-14 07:39:10 +1100

    feat: ✨ Add helper to count the number of decimal digits in a number

--- Aoc2024/Utilities/MathPlus.cs
+++ Aoc2024/Utilities/MathPlus.cs
@@ -1,4 +1,4 @@
-﻿namespace Aoc2024.Utilities;
+namespace Aoc2024.Utilities;
 
 public static class MathPlus
 {
@@ -31,6 +31,20 @@ public static class MathPlus
         }
 
         return res;
+    }
+
+    public static int DigitCount(this long value)
+    {
+        int digits = 1;
+        long pow10 = 10;
+
+        while (pow10 <= value)
+        {
+            ++digits;
+            pow10 *= 10;
+        }
+
+        return digits;
     }
 
     public static long SumRange(long start, long count) => count * (2 * start + count - 1) / 2;
